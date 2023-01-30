242f790c40d17849a6a2a6fbb80c4f9f58fb1c96
aumitcelik <aumitcelik@gmail.com>
2023-01-30 11:01:01 +0300

    update

---
+++ encapsulation/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap newkitap;
+            newkitap.setSayfaSayisi(55);
+        }
+    }
+}
